659e37c6a002a4c2738c5ed6205a2b6ca23d970a
joaorolim <pjrolim@yahoo.com.br>
2023-12-05 03:49:33 -0300

    Adicionado SHEBANG

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+
+## !/usr/bin/python3
+
+## !/usr/bin/env python3
+
+# A primeira linha é chamada de SHEBANG
+# Serve para passar informações para o SO, por exemplo, 
+# qual python será rodado.
+
+# Uma alterantiva, seria colocar como está na linha 5.
+# Dessa maneira, vc força a passagem das variáveis de ambiente para o python. 
+
+# Este programa imprime Hello World
 print("Hello, World!")
-print('João'.upper())
-print(56 + 7)
+
+# print('João'.upper())
+# print(56 + 7)
