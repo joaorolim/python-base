08973282fd526332e5d2c5ff53c5c449ab8e62f2
joaorolim <pjrolim@yahoo.com.br>
2024-01-17 08:36:59 -0300

    prefixcalc2.py com Try/Except

--- prefixcalc2.py
+++ prefixcalc2.py
@@ -70,7 +70,11 @@ for num in nums:
 
     validated_nums.append(num)
 
-n1, n2 = validated_nums
+try:
+    n1, n2 = validated_nums
+except ValueError as e:
+    print(f"{str(e)}")
+    sys.exit(1)
 
 if operation == "sum":
     result = n1 + n2
@@ -85,13 +89,18 @@ elif operation == "div":
     else:
         result = n1 / n2
 
+print(f"O resultado é {result}") 
 
+# path = "/"
 path = os.curdir
 filepath = os.path.join(path, "prefixcalc2.log")
 timestamp = datetime.now().isoformat()
 user = os.getenv('USER','anonymous')
 
-with open(filepath, "a") as file_:
-    file_.write(f"{timestamp} - {user}: {operation},{n1},{n2} = {result}\n")
-
-print(f"O resultado é {result}") 
+try:
+    with open(filepath, "a") as file_:
+        file_.write(f"{timestamp} - {user}: {operation},{n1},{n2} = {result}\n")
+except PermissionError as e:
+    # TODO: logging
+    print(f"{str(e)}")
+    sys.exit(1)
